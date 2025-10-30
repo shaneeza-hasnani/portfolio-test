4dbf64b178438196ab499014283d9fc3a87b1e53
Shaneeza Hasnani <154279129+shaneeza-hasnani@users.noreply.github.com>
2025-10-30 17:40:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // 👇 this line fixes the GitHub Pages white screen
-  base: "/portfolio/",
-  
   server: {
     host: "::",
     port: 8080,
